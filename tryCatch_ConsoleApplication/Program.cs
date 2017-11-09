e2958260339fd7743f7703db17037d77c46fd03c
programmiertier <zimmermannveronique@protonmail.com>
2017-11-09 10:10:07 +0100

    tryCatch

--- tryCatch_ConsoleApplication/Program.cs
+++ tryCatch_ConsoleApplication/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace tryCatch_ConsoleApplication
 {
+    class A
+    {
+        public int zahl;
+        public string kette;
+        public int[] zahlen = new int[5];
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            A abject = new A();
+
+            WriteLine(abject.zahl);
+            WriteLine(abject.kette);
+
         }
     }
 }
